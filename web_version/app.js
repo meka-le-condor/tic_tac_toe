cf6c9008b0d4e2bccef1742fa8335b33867d3d29
meka-le-condor <kamohamed674@gmail.com>
2024-05-13 18:07:29 +0000

    VT

---
+++ web_version/app.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const startBtn = document.getElementById("startBtn");
+  
+    startBtn.addEventListener("click", function() {
+      // Rediriger vers la page du jeu
+      window.location.href = "niv.html";
+    });
+
+    const startBtnfac = document.getElementById("startBtnfac");
+  
+    startBtnfac.addEventListener("click", function() {
+      // Rediriger vers la page du jeu niveau facile
+      window.location.href = "facile.html";
+    });
+
+    const startBtndif = document.getElementById("startBtndif");
+  
+    startBtndif.addEventListener("click", function() {
+      // Rediriger vers la page du jeu niveau difficile
+      window.location.href = "index.html";
+    });
+  });
+  
